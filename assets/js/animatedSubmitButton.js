68221be24a362480ef3e7003f23ff837bf28ffd4
admin <admin@ETUDIANT.local>
2020-09-07 16:44:58 +0200

    add jquery validation

--- assets/js/animatedSubmitButton.js
+++ assets/js/animatedSubmitButton.js
@@ -5,7 +5,7 @@ import('jquery');
  */
 var animateButton = function(e) {
 
-    e.preventDefault;
+    e.preventDefault();
     //reset animation
     e.target.classList.remove('animate');
     
@@ -15,11 +15,27 @@ var animateButton = function(e) {
     
     setTimeout(function(){
       e.target.classList.remove('animate');
-    },1500);
+    },2500);
   };
   
   var classname = document.getElementsByClassName("buttonContact");
   
   for (var i = 0; i < classname.length; i++) {
+    console.log(classname);
     classname[i].addEventListener('click', animateButton, false);
   }
+
+  $(document).ready(function(){
+    $("#contactForm").validate({
+        submitHandler:function(form){
+            if (this.valid()){
+              $('#submit').addClass('success');
+              form.submit();
+            }
+            else {
+              $('#submit').addClass('error');
+              form.submit();
+            } 
+        }  
+    });
+});
